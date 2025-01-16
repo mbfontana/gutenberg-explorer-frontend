ed837b0d2ffdf6c9dfa3b20e0744b4a080c44de7
Matheus Fontana <mbfontana@outlook.com>
2025-01-16 11:12:52 -0300

    dummy token

--- src/api/AuthenticatedAxios.ts
+++ src/api/AuthenticatedAxios.ts
@@ -4,7 +4,8 @@ const createClient = (baseURL: string) => {
   const client = axios.create({ baseURL });
 
   client.interceptors.request.use((config: any) => {
-    config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+    config.headers.Authorization =
+      "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJpc3MiOiJQcm9qZWN0IEd1dGVuYmVyZyBFeHBsb3JlciIsImlhdCI6MTczNzAzNTc4NSwiZXhwIjoxNzY4NTcxNzg1LCJhdWQiOiJsb2NhbGhvc3QuY29tIiwic3ViIjoiam9obmRvZUBleGFtcGxlLmNvbSIsIkdpdmVuTmFtZSI6IkpvaG5ueSIsIlN1cm5hbWUiOiJEb2UiLCJFbWFpbCI6ImpvaG5kb2VAZXhhbXBsZS5jb20iLCJSb2xlIjoiVXNlciJ9.6G8-zRCzheoIDLWnT8fph7FxQNQIhva3dPNwVoPmhwM";
     return config;
   });
 
